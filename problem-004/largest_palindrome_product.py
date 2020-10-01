89c523b04c459361a179c369c85e8c5de32ce03e
satyajitghana <satyajitghana@users.noreply.github.com>
2020-10-01 15:22:54 +0000

    deploy: e484d2bfbe1eb6b4367d4752ac1225fca3a5fa43

---
+++ problem-004/largest_palindrome_product.py
@@ -0,0 +1,19 @@
+def is_palindrome(num):
+	num_as_str = str(num)
+	len_of_num = len(num_as_str)
+	for i in range(len_of_num / 2):
+		if num_as_str[i] != num_as_str[len_of_num - 1 - i]:
+			return False
+	return True
+
+
+def solution():
+	products = []
+	for factor1 in range(999, 99, -1):
+		for factor2 in range(999, 99, -1):
+			product = factor1 * factor2
+			if is_palindrome(product):
+				products.append(product)
+	return max(products)
+
+print(solution())
