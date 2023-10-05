cad074bd60c2a5ab428c21ca460a742a90de22b8
Satyajit Ghana <satyajitghana7@gmail.com>
2023-10-05 18:48:05 +0000

    add: solution 43

---
+++ solutions/problem-043/sub_string.py
@@ -0,0 +1,17 @@
+from itertools import *
+
+
+def has_property(digits):
+    primes = [1, 2, 3, 5, 7, 11, 13, 17]
+    for i in range(1, len(digits) - 2):
+        if not int("".join(digits[i : i + 3])) % primes[i] == 0:
+            return False
+    return True
+
+
+def convert_list_to_int(l):
+    return int("".join(str(i) for i in l))
+
+
+pandigitals = permutations((str(i) for i in range(10)))
+print(sum(convert_list_to_int(p) for p in pandigitals if has_property(p)))
